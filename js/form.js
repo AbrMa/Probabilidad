cf6bf7b6291a1e718b31c9140b3a4bd78f7b42a4
AbrMa <abrma98@gmail.com>
2020-11-03 13:01:03 -0600

    All validations, probability calculation bug

--- js/form.js
+++ js/form.js
@@ -1,25 +1,29 @@
-function generateBoxes() {
+function validate(n) {
+    if (isNaN(n) || n <= 0) {
+        swal({
+            icon: "error",
+            title: "Datos invalidos"
+        })
+        return false;
+    }
+    return true;
+}
 
+function generateBoxes() {
 
     let totalfeilds = document.getElementById("colorNumber").value;
 
-    if (isNaN(totalfeilds) || totalfeilds <= 0) {
-        swal({
-            icon: "error",
-            title: "Dato num. de colores invalido"
-        })
-    }
-    else {
+    if (validate(totalfeilds) === true) {
         swal({
             icon: "info",
-            title: "El num de colores generado es " + totalfeilds
+            title: "El num. de casillas a generar es " + totalfeilds
         })
         let marbleColors = "";
         let marbleColorsSelect = '<select class="form-control selectButtonColor" id="selectedColor"> <option id = "SC">Seleccione Color</option>';
         let i;
         for (i = 1; i <= totalfeilds; i++) {
             marbleColors += '<div class="form-label-group"> <input type="text" id="num' + i + '" class="form-control" placeholder="N&uacute;mero de canicas del color ' + i + '" required autofocus><label for="num' + i + '">N&uacute;mero de canicas de color ' + i + '</label></div>'
-            marbleColorsSelect += '<option id = "color'+ i +'">' + i + '</option>';
+            marbleColorsSelect += '<option id = "color' + i + '">' + i + '</option>';
         }
         marbleColors += '<hr> <div class="form-label-group"> <input type="text" id="extractedMarbles" class="form-control" placeholder="N&uacute;mero de colorNumber" required autofocus><label for="extractedMarbles">N&uacute;mero de canicas a extraer</label></div>';
         marbleColorsSelect += '</select> ';
@@ -30,25 +34,39 @@ function generateBoxes() {
 
 function calculate() {
     let MAX = document.getElementById("colorNumber").value;
-    let i;
-    let probability = 1;
-    let totalMarbles = 0;
-    let selectedMarblesColor = document.getElementById("selectedColor").value;
-    let numberSelectedMarblesColor = document.getElementById("num"+selectedMarblesColor).value;
-    let extracredMarbles = document.getElementById("extractedMarbles").value;
-    for (i = 1; i <= MAX; i++) {
-        let numid = "num" + i.toString();
-        totalMarbles += parseInt(document.getElementById(numid).value);
+    if (validate(MAX) === true) {
+        let i;
+        let probability = 1;
+        let totalMarbles = 0;
+        let extracredMarbles = document.getElementById("extractedMarbles").value;
+        let selectedMarblesColor = document.getElementById("selectedColor").value;
+        if (validate(selectedMarblesColor) === true && validate(selectedMarblesColor) === true && validate(extracredMarbles) === true) {
+            let numberSelectedMarblesColor = document.getElementById("num" + selectedMarblesColor).value;
+            let answerExists = true;
+            for (i = 1; i <= MAX; i++) {
+                let numid = "num" + i.toString();
+                let currentNumber = document.getElementById(numid).value;
+                if (validate(currentNumber) === true) {
+                    totalMarbles += currentNumber;
+                }
+                else {
+                    answerExists = false;
+                    break;
+                }
+            }
+            if (answerExists === true) {
+                //probability -= (combinatorial(numberSelectedMarblesColor, 0) * combinatorial(totalMarbles - extracredMarbles, extracredMarbles)) / combinatorial(totalMarbles, extracredMarbles);
+                swal({
+                    icon: "success",
+                    title: "La probabilidad es de  " + probability
+                })
+            }
+        }
     }
-    swal({
-        icon: "info",
-        title: extracredMarbles
-    })
-
 }
 
 function combinatorial(n, k) {
-    return (factorial(n))/(factorial(k)*factorial(n-k));
+    return (factorial(n)) / (factorial(k) * factorial(n - k));
 }
 
 function factorial(n) {
