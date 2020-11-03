3636510c631dab7aa42ece3cc627b615a8fc4c2e
AbrMa <abrma98@gmail.com>
2020-11-02 22:44:01 -0600

    Fixed form for n boxes

--- js/form.js
+++ js/form.js
@@ -1,6 +1,27 @@
-function test() {
-    swal({
-        icon: "success",
-        title: "El resultado es",
-      })
-  }
+function generateBoxes() {
+
+
+    var totalfeilds = document.getElementById("colores").value;
+
+    if (isNaN(totalfeilds) || totalfeilds <= 0) {
+        swal({
+            icon: "error",
+            title: "Dato num. de colores invalido"
+        })
+    }
+    else {
+        swal({
+            icon: "info",
+            title: "El num de colores generado es " + totalfeilds
+        })
+        var htmlString = "";
+
+        var i;
+
+        for (i = 1; i <= totalfeilds; i++) {
+            htmlString += '<div class="form-label-group"> <input type="text" id="num' + i + '" class="form-control" placeholder="N&uacute;mero de canicas de color ' + i + '" required autofocus><label for="num' + i + '">N&uacute;mero de canicas de color ' + i + '</label></div>'
+        }
+
+        document.getElementById("colors").innerHTML = htmlString;
+    }
+}
