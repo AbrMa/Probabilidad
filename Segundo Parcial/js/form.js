a8bde1eb303c85ee00a979d94c08a0143c810dc5
AbrMa <abrma98@gmail.com>
2020-12-12 22:37:28 -0600

    Datos

--- Segundo Parcial/js/form.js
+++ Segundo Parcial/js/form.js
@@ -1,29 +1,28 @@
-function validate(n) {
+function validate(n, dato) {
     if (isNaN(n) || n < 0) {
         swal({
             icon: "error",
-            title: "Datos invalidos"
+            title: "Dato " + dato + " invalido"
         })
         return false;
     }
     return true;
 }
 
-function validateMarbels(n, total) {
-    if (n <= total) {
-        return true;
-    }
-    else {
-        swal({
-            icon: "error",
-            title: "El num de canicas tomadas no puede ser mayor a la suma de las canicas"
-        })
-        return false;
-    }
-}
 
-function calculate() {
-    
+function calculate() {  
+    let totalComputers = document.getElementById("totalComputers").value;
+    let defectiveComputes = document.getElementById("defectiveComputers").value;
+    let chosenComputers = document.getElementById("chosenComputers").value;
+    /*
+    if (validate(totalComputers, "Total computadoras") === true && validate(defectiveComputes, "Computadoras defectuosas") === true && validate(chosenComputers, "Computadoras escogidas") === true) {
+
+    }
+    */
+   /*
+   let con1 = validate(totalComputers, "Total computadoras");
+   let con2 = validate(defectiveComputes, "Computadoras defectuosas");
+   */
 }
 
 function combinatorial(n, k) {
