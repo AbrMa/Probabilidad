0824c58ebebb65ba68af0bd3d98d1af8ce70a5cf
AbrMa <abrma98@gmail.com>
2020-12-13 19:21:03 -0600

    Validaciones

--- Segundo Parcial/js/form.js
+++ Segundo Parcial/js/form.js
@@ -14,13 +14,32 @@ function calculate() {
     let totalComputers = document.getElementById("totalComputers").value;
     let defectiveComputers = document.getElementById("defectiveComputers").value;
     let chosenComputers = document.getElementById("chosenComputers").value;
-    let chosenProbability = document.getElementById("chosenProbability").value;
-    let probability = combinatorial(defectiveComputers,chosenProbability) * combinatorial(totalComputers-defectiveComputers,chosenComputers-chosenProbability);
-    probability = probability / combinatorial(totalComputers,chosenComputers);    
-    /*if (validate(totalComputers, "total de computadoras") && validate(defectiveComputers, "computadoras defectuosas") && validate(chosenComputers, "computadoras escogidas") && validate(chosenProbability, "probabilidad elegida") ) {
-        //  ejemplo
-        
-    }*/
+    let chosenProbability = document.getElementById("chosenProbability").value;    
+    if (validate(totalComputers, "total de computadoras") && validate(defectiveComputers, "computadoras defectuosas") && validate(chosenComputers, "computadoras escogidas") && validate(chosenProbability, "probabilidad elegida") ) {
+        if (defectiveComputers <= totalComputers && chosenComputers <= totalComputers && chosenProbability <= defectiveComputers) {
+            let probability = combinatorial(defectiveComputers,chosenProbability) * combinatorial(totalComputers-defectiveComputers,chosenComputers-chosenProbability);
+            probability = probability / combinatorial(totalComputers,chosenComputers);
+            probability.toString();
+            if (probability === 0) {
+                swal({
+                    icon: "success",
+                    title: "0"
+                }) 
+            }
+            else {
+                swal({
+                    icon: "success",
+                    title: probability
+                }) 
+            }
+        }
+        else {
+            swal({
+                icon: "error",
+                title: "Las computadoras escogidas/defectuosas deben ser menores o iguales a las totales "
+            }) 
+        }
+    }
 }
 
 function combinatorial(n, k) {
