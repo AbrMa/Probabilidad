9e7c044f4b50e13cf7ebe48cc5a254ad271fb0bc
AbrMa <abrma98@gmail.com>
2021-01-18 12:35:47 -0600

    now bi calculates from 0 to n and stores the answers in an array

--- Tercer Parcial/js/form.js
+++ Tercer Parcial/js/form.js
@@ -14,13 +14,18 @@ function calculate() {
     let x = document.getElementById("x").value;
     let n = document.getElementById("n").value;
     let p = document.getElementById("p").value;
-    let q = 1 - p;   
+    let q = 1 - p;
+    let answer = [];   
     if (validate(x, "Dato x invalido") && validate(n, "Dato n invalido") && validate(p, "Dato p invalido") && validate(q, "p debe estar entre 0 y 1") && validate(n - x, "x debe ser menor o igual a n")) {
-        let bi = combinatorial(n,x) * Math.pow(p, x) * Math.pow(q, n - x);
-        swal({
-            icon: "success",
-            title: bi
-        })
+        let sum = 0;
+        for (let i = 0; i <= n; i++) {
+            let bi = combinatorial(n,i) * Math.pow(p, i) * Math.pow(q, n - i);
+            answer.push(bi);
+            sum += bi;
+        }
+        alert("valores (bi) de 0 hasta n " + answer );
+        alert("valor (bi) en x " + answer[x]);
+        alert("suma de todas las (bi)  " + sum);
     }
 }
 
