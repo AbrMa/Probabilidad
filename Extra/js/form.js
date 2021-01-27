6c372485deb1db49e9edb4d14659a5b17470aed2
AbrMa <abrma98@gmail.com>
2021-01-26 18:27:38 -0600

    getValues function and some validations in the generate fields inputs

--- Extra/js/form.js
+++ Extra/js/form.js
@@ -1,19 +1,9 @@
-function validate(n, msj) {
-    if (isNaN(n) || n < 0) {
-        swal({
-            icon: "error",
-            title: msj
-        })
-        return false;
-    }
-    return true;
-}
-
 function generateBoxes()
 {
     let totalfeilds = document.getElementById("numberAirstrip").value;
-    if (validate(totalfeilds) === true) {
-        if (totalfeilds > 0) {
+    let n = parseInt(document.getElementById("totalPlanes").value);
+    if (n >= 0 && !isNaN(n)) { 
+        if (totalfeilds > 0 && !isNaN(totalfeilds)) {
             swal({
                 icon: "info",
                 title: "El num. de casillas a generar es " + totalfeilds
@@ -22,8 +12,8 @@ function generateBoxes()
             let avionesPista = "";
             let i;
             for (i = 1; i <= totalfeilds; i++) {
-                probabilidadPista += '<div class="form-label-group"> <input type="text" id="pista' + i + '" class="form-control" placeholder="Probabilidad de la pista ' + i + '" required autofocus><label for="pista' + i + '">Probabilidad de la pista ' + i + '</label></div>';
-                avionesPista += '<div class="form-label-group"> <input type="text" id="avion' + i + '" class="form-control" placeholder="Aviones en la pista ' + i + '" required autofocus><label for="avion' + i + '">Aviones en la pista ' + i + '</label></div>';
+                probabilidadPista += '<div class="form-label-group"> <input type="text" id="airstrip' + i + '" class="form-control" placeholder="Probabilidad de la pista ' + i + '" required autofocus><label for="airstrip' + i + '">Probabilidad de la pista ' + i + '</label></div>';
+                avionesPista += '<div class="form-label-group"> <input type="text" id="plane' + i + '" class="form-control" placeholder="Aviones en la pista ' + i + '" required autofocus><label for="plane' + i + '">Aviones en la pista ' + i + '</label></div>';
             }
             document.getElementById("probabilidades").innerHTML = probabilidadPista;
             document.getElementById("avionesPista").innerHTML = avionesPista;
@@ -31,15 +21,59 @@ function generateBoxes()
         else {
             swal({
                 icon: "error",
-                title: "El num. de casillas debe ser mayor  a 0"
+                title: "El num. de pistas debe ser mayor a 0"
             })
         }
+    }
+    else {
+        swal({
+            icon: "error",
+            title: "El num. de aviones debe ser mayor o igual a 0"
+        })
     }
 }
 
 function getValues()
 {
-    calculate(x,p);
+    let x = [];
+    let p = [];
+    let pistas = parseInt(document.getElementById("numberAirstrip").value);
+    let totalPlanes = parseInt(document.getElementById("totalPlanes").value);
+    let sumPlanes = 0;
+    for (var i = 1; i <= pistas; i++) {
+        let airstripID = "airstrip" + i.toString();
+        let planeID = "plane" + i.toString();
+        let currentPlane = eval(document.getElementById(planeID).value);
+        let currentAirstrip = eval(document.getElementById(airstripID).value);
+        if (0 <= currentAirstrip && currentAirstrip <= 1) {
+            if (currentPlane >= 0) {
+                x.push(currentPlane);
+                p.push(currentAirstrip);
+                sumPlanes += currentPlane;
+            }
+            else {
+                swal({
+                    icon: "error",
+                    title: "Los aviones en la pista " + i + " deben ser un num. mayor a 0"
+                })
+            }
+        }
+        else {
+            swal({
+                icon: "error",
+                title: "La probabilidad de la pista " + i + " debe ser un num. entre 0 y 1"
+            })
+        }
+    }
+    if (sumPlanes === totalPlanes) {
+        calculate(x,p);
+    }
+    else {
+        swal({
+            icon: "error",
+            title: "La suma de los aviones no es igual a " + totalPlanes
+        })
+    }
 }
 
 function calculate(x,p) {
@@ -56,7 +90,7 @@ function calculate(x,p) {
     probabilidad = probabilidad * n;
     swal(
         {
-            icon: "succes",
+            icon: "success",
             text:"La probabilidad es : "+ probabilidad
     });
 }
